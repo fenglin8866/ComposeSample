537391c3f6354c2ec640baee867b7630e3e60562
xxh <fenglin8866@163.com>
2023-07-12 14:50:44 +0800

    更新jetSurvey

--- app/src/main/java/com/main/AppNavHost.kt
+++ app/src/main/java/com/main/AppNavHost.kt
@@ -20,6 +20,7 @@ import com.codelabs.basiclayouts.BasicLayoutsActivity
 import com.codelabs.basics.BasicActivity
 import com.codelabs.state.StateActivity
 import com.codelabs.theming.ui.start.ThemeStartActivity
+import com.example.compose.jetsurvey.SurveyActivity
 import com.example.crane.home.CraneActivity
 import com.example.jetcaster.ui.CasterActivity
 import com.example.jetnews.ui.JetnewsActivity
@@ -28,7 +29,6 @@ import com.example.reply.ui.ReplyActivity
 import com.samples.jetchat.ChatActivity
 import com.samples.jetlagged.LaggedActivity
 import com.samples.jetsnack.ui.SnackActivity
-import com.samples.jetsurvey.SurveyActivity
 import com.samples.owl.ui.OwlActivity
 import com.xxh.sample.TestDemoScreen
 
